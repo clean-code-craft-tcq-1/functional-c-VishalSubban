1fe79558baed65dc56977c77da2c3cbf3a96d2e9
Vishal Subban <Subban.Vishal@in.bosch.com>
2021-03-04 01:03:28 +0530

    Change1

---
+++ monitor.c
@@ -0,0 +1,51 @@
+#include "monitor.h"
+
+
+int isBatteryTempHealthy(float temperature)
+{
+    if(temperature < UNDER_TEMPERATURE || temperature > OVER_TEMPERATURE)
+    {
+        printf("Battery not in nominal temperature !\n");
+        return 0;
+    }
+    return 1;
+}
+
+int isBatterySocHealthy(float stOfCharge)
+{
+    if(stOfCharge < MIN_SOC_AREA || stOfCharge > MAX_SOC_AREA)
+    {
+        printf("Battery not in proper State of Charge !\n");
+        return 0;
+    }
+    return 1;
+}
+
+int isBatteryChargingOK(float current, float voltage)
+{
+    if(chargeRate > OVER_CURRENT) 
+    {
+        printf("Batter not under Charge Rate !\n");
+        return 0;
+    }
+    else if(voltage < UNDER_VOLTAGE || voltage > OVER_VOLTAGE)
+    {
+        printf("Voltage fluctuated!\n");
+        return 0;
+    }
+    return 1;
+}
+
+int main() {
+  assert(isBatteryTempHealthy(25));
+  assert(!isBatteryTempHealthy(50));
+  
+  assert(isBatterySocHealthy(60));
+  assert(!isBatterySocHealthy(100));
+  
+  assert(isBatteryChargingOK(0.56, 15));
+  assert(!isBatteryChargingOK(2, 20));
+  assert(!isBatteryChargingOK(2, 15));
+  assert(!isBatteryChargingOK(0.66, 1.2));
+  assert(!isBatteryChargingOK(0.66, 19));
+}
